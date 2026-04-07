9089b26e430aedf843404f4563ef5adef2c6e19b
snr-roko <rbbagyei@outlook.com>
2026-04-07 15:22:58 +0000

    add layout for auth directory to ensure proper routing

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router"
+
+const AuthRootLayout = () => {
+
+    return (
+        <Stack screenOptions={{
+            headerShown: false
+        }}>
+            <Stack.Screen
+                name="login"
+            />
+        </Stack>
+    )
+}
+
+export default AuthRootLayout
