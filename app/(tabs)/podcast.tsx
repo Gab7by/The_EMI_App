a3ed6519a0845669bf3af223d745ec0901e5bab8
snr-roko <rbbagyei@outlook.com>
2026-04-14 12:33:21 +0000

    feat: configure tabs with a custom tab component

---
+++ app/(tabs)/podcast.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const PodcastScreen = () => {
+    return (
+        <SafeAreaView className="flex-1 bg-menorah-bg">
+
+        </SafeAreaView>
+    )
+}
+
+export default PodcastScreen
