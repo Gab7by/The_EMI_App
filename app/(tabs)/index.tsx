92908194f834fc57adf991b5f8f73f12055152c3
snr-roko <rbbagyei@outlook.com>
2026-04-18 15:12:08 +0000

    feat: update layout in Home component for improved spacing and consistency

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,13 +12,13 @@ import { SafeAreaView } from "react-native-safe-area-context"
 const Home = () => {
   
   return (
-    <SafeAreaView className="flex-1 px-4 py-8 gap-7 bg-menorah-bg">
+    <SafeAreaView className="flex-1 gap-7 px-4 py-8 bg-menorah-bg">
       <HomeProfileBar />
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName="gap-6">
         <ImageSlider items={imageItems} height={220} />
-        <View>
-          <Text className="text-xl pl-7 font-bold text-left text-[#EFBF04]">The Menorah</Text>
-          <View className="items-left justify-center bg-[#C6FF00] pl-7 rounded-3xl self-center w-full max-w-sm h-80">
+        <View className="gap-5">
+          <Text className="text-xl font-bold text-[#EFBF04]">The Menorah</Text>
+          <View className="justify-center bg-[#C6FF00] p-7 rounded-3xl w-full ">
             <View className="mb-7">
               <Sparkling width={25} height={25} />
             </View>
