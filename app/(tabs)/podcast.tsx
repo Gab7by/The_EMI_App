234589e0692e66c81ceb0d6ee6b199376bcc93cb
snr-roko <rbbagyei@outlook.com>
2026-04-18 08:39:13 +0000

    feat: add ImageSlider component with slider functionality and integrate into PodcastScreen

--- app/(tabs)/podcast.tsx
+++ app/(tabs)/podcast.tsx
@@ -6,6 +6,8 @@ import { Button } from "@/components/ui/button"
 import { View } from "react-native"
 import { useLiveStreamStartModalStore } from "@/store/podcast-store"
 import LiveStreamStartModal from "@/components/podcast/liveStreamStartModal"
+import { SliderItem } from "@/types/ui-commons-props"
+import ImageSlider from "@/components/commons/image-slider"
 
 const PodcastScreen = () => {
 
@@ -15,9 +17,19 @@ const PodcastScreen = () => {
         setOpenLiveStreamStartModal(true)
     }
 
+    const imageItems: SliderItem[] = [
+        {id: "1", source: require("@/assets/pictures/slider-image-1.png")},
+        {id: "2", source: require("@/assets/pictures/slider-image-2.png")},
+        {id: "3", source: require("@/assets/pictures/slider-image-3.png")},
+        {id: "4", source: require("@/assets/pictures/slider-image-4.png")},
+        {id: "5", source: require("@/assets/pictures/slider-image-5.png")},
+        {id: "6", source: require("@/assets/pictures/slider-image-6.png")}
+    ]
+
     return (
-        <SafeAreaView className="flex-1 bg-menorah-bg py-8 px-4 gap-3 relative">
+        <SafeAreaView className="flex-1 bg-menorah-bg py-8 px-4 gap-4 relative">
             <PodcastProfileBar />
+            <ImageSlider items={imageItems}  height={220}/>
             <LiveStreamCard 
                 hostName="Rev. Dr.Seth Owusu" 
                 playlist="Lunch Prayer Fire"
--- components/commons/image-slider.tsx
+++ components/commons/image-slider.tsx
@@ -1,5 +1,71 @@
-const ImageSlider = () => {
+import { SliderProps } from "@/types/ui-commons-props"
+import { Image } from "expo-image"
+import { useCallback, useRef, useState } from "react"
+import { NativeScrollEvent, NativeSyntheticEvent, ScrollView, View } from "react-native"
 
+const ImageSlider = ({items, height = 200}:SliderProps) => {
+
+    const [activeIndex, setActiveIndex] = useState(0)
+    const [sliderWidth, setSliderWidth] = useState(0)
+
+    const scrollRef = useRef<ScrollView>(null)
+
+    const handleLayout = useCallback(({nativeEvent}:{nativeEvent: {layout: {width: number}}}) => {
+        setSliderWidth(nativeEvent.layout.width)
+    }, [])
+
+    const handleScroll = useCallback((event: NativeSyntheticEvent<NativeScrollEvent>) => {
+        if (!sliderWidth) return
+
+        const scrollPosition = event.nativeEvent.contentOffset.x
+
+        const index = Math.round(scrollPosition / sliderWidth)
+
+        setActiveIndex(index)
+    }, [sliderWidth])
+
+    return (
+        <View className="gap-3">
+            <View className="rounded-3xl overflow-hidden">
+                <ScrollView
+                    showsHorizontalScrollIndicator={false}
+                    onScroll={handleScroll}
+                    onLayout={handleLayout}
+                    ref={scrollRef}
+                    pagingEnabled
+                    horizontal
+                    scrollEventThrottle={16}
+                    style={{height}}
+                    className="w-full"
+                >
+                    {
+                        items.map((item) => (
+                            <Image
+                                key={item.id}
+                                source={item.source}
+                                contentFit="cover"
+                                style={{height, width: sliderWidth}}
+                            />
+                        ))
+                    }
+                </ScrollView>
+            </View>
+            <View className="flex-row justify-center items-center gap-2">
+                {items.map((_, index) => (
+                    <View
+                        key={index}
+                        className={
+                            `rounded-full transition-all ${
+                            index === activeIndex
+                                ? 'w-4 h-2 bg-menorah-primary'      
+                                : 'w-2 h-2 bg-menorah-primary/40'
+                            }`
+                        }
+                    />
+                ))}
+            </View>
+        </View>
+    )
 }
 
 export default ImageSlider
